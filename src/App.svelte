dded9900e748e086716fc0236d1cca3f9076ac10
Ricardo Maia <rsmaia@gmail.com>
2020-11-05 11:53:27 -0300

    bootstrap

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,13 @@
 <script>
   import StatusPage from "./components/StatusPage.svelte";
+  import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
 </script>
 
+<svelte:head>
+  <script async defer src="https://buttons.github.io/buttons.js">
+  </script>
+</svelte:head>
+<Header />
 <StatusPage />
 <Footer />
---
+++ src/components/Header.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Button } from "sveltestrap";
+</script>
+
+<div>
+  <Button primary>primary</Button>
+</div>
+<div id="header">Header</div>
