7b57a9c9229c4c1c7b242764a8a022ebd2c5c42e
Ricardo Maia <rsmaia@gmail.com>
2020-11-28 21:56:01 -0300

    🚧 new features in setup script

--- setup.js
+++ setup.js
@@ -1,75 +1,138 @@
+/**
+ * @author Ricardo Maia
+ * @license GPLv3+
+ *
+ * @see https://github.com/octokit/core.js#rest-api-example
+ * @see https://docs.github.com/en/free-pro-team@latest/rest/reference/issues#create-a-label
+ */
+
+const chalk = require("chalk");
 const { Octokit } = require("@octokit/core");
-
-var fs = require("fs"),
-  ini = require("ini");
-
-var config = ini.parse(fs.readFileSync("./.git/config", "utf-8"));
-
-console.log(config['remote "origin"']["url"]);
-
+const fs = require("fs");
+const ini = require("ini");
 const readline = require("readline");
+const { exit } = require("process");
+const { compute_slots } = require("svelte/internal");
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.write("Create your access token at https://github.com/settings/tokens\n");
+// Read local .git/config file
+let config = ini.parse(fs.readFileSync("./.git/config", "utf-8"));
 
-rl.question("Username: ", function (username) {
-  rl.question("Personal access token: ", function (token) {
-    const octokit = new Octokit({
-      auth: token, //"b8ae2aad0d7560eb8d94f69d48f8bb8049a78a48",
-    });
+// Extract user and repostory names
+const regex = /github.com\/(?<username>.*)\/(?<repo>.*)\.git/;
+const remote_origin = config['remote "origin"']["url"];
+let git_info = regex.exec(remote_origin);
+let username = git_info.groups["username"];
+let repo = git_info.groups["repo"];
 
-    // Get user data
-    const result = octokit.request("/user");
+// Display git properties
+//console.log("remote_origin: ", remote_origin);
 
-    //let labels = getLabels(api);
+const banner = `
+ ██████╗ ██╗  ██╗ ██████╗ ███████╗████████╗
+██╔════╝ ██║  ██║██╔═══██╗██╔════╝╚══██╔══╝
+██║  ███╗███████║██║   ██║███████╗   ██║   
+██║   ██║██╔══██║██║   ██║╚════██║   ██║   
+╚██████╔╝██║  ██║╚██████╔╝███████║   ██║   
+ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   
+                                           
+███████╗███████╗████████╗██╗   ██╗██████╗  
+██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗ 
+███████╗█████╗     ██║   ██║   ██║██████╔╝ 
+╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝  
+███████║███████╗   ██║   ╚██████╔╝██║      
+╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝   
+`;
 
-    result.then((response) => {
-      console.log(response.data);
-    });
+console.clear();
+console.log(banner);
 
-    //console.log(`Username: ${username} and token: ${token}`);
+console.log(chalk.yellow("username: "), chalk.blue(username));
+console.log(chalk.yellow("repo: "), chalk.blue(repo));
 
-    rl.close();
-  });
-});
+// Await user input for access token
+console.log(
+  chalk.magenta(
+    "Create your access token at https://github.com/settings/tokens"
+  )
+);
 
-/**
- * https://github.com/octokit/core.js#rest-api-example
- * https://docs.github.com/en/free-pro-team@latest/rest/reference/issues#create-a-label
- */
-
-// Authentication
-function auth(token) {
+rl.question(chalk.yellow("Personal access token: "), function (token) {
   const octokit = new Octokit({
-    auth: token, //"b8ae2aad0d7560eb8d94f69d48f8bb8049a78a48",
-  });
-  return octokit;
-}
-
-// List labels
-async function getLabels(api) {
-  const result = api.request("GET /repos/ricardomaia/statuspage/labels", {
-    owner: "ricardomaia",
-    repo: "statuspage",
+    auth: token, //"d58f34fc66b53bb5b0167b1ef7b794f4baedb87e",
   });
 
-  result.then((response) => {
-    return response.data;
-  });
-}
+  // Get user info
+  /*
+  const result = octokit
+    .request("/user")
+    .then((response) => {
+      console.log(chalk.green("User authentication successful!"));
+    })
+    .catch(function (error) {
+      console.log(chalk.red(error.message));
+    });
+*/
+
+  // Get repo labels
+
+  let repo_labels = [];
+  let ghost_labels = [
+    "statuspage",
+    "incident",
+    "operational",
+    "partial outage",
+    "major outage",
+    "performance issues",
+    "foobar",
+  ];
+  const result2 = octokit
+    .request(`GET /repos/${username}/${repo}/labels`, {
+      owner: username,
+      repo: repo,
+    })
+    .then((response) => {
+      response.data.forEach((element) => {
+        repo_labels.push(element.name);
+      });
+
+      // Verify if ghost labels are present in repository
+
+      ghost_labels.forEach((label) => {
+        if (repo_labels.includes(label)) {
+          console.log(`Label "${chalk.green(label)}" found!`);
+        } else {
+          console.log(`Label "${chalk.red(label)}" NOT found!`);
+        }
+      });
+    })
+    .catch((error) => {
+      console.log(chalk.red(error.message));
+    });
+
+  rl.close();
+});
 
 // Create a label
 function createLabels(api) {
-  const result = api.request("POST /repos/{owner}/{repo}/labels", {
-    owner: "octocat",
-    repo: "hello-world",
-    name: "name",
-  });
-
-  result.then((response) => {
-    console.log(response.data);
-  });
+  try {
+    const result = api
+      .request("POST /repos/{owner}/{repo}/labels", {
+        owner: "octocat",
+        repo: "hello-world",
+        name: "name",
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(chalk.red(error.message));
+      });
+  } catch (error) {
+    console.log(chalk.red(error.message));
+  }
 }
