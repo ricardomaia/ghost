d2c8113914ea9c4d69ea938ca835c90a218ac7a4
Ricardo Maia <rsmaia@gmail.com>
2021-09-29 21:06:27 -0300

    Closes #23

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,12 @@
-<script async defer src="https://buttons.github.io/buttons.js">
-</script>
+<div class="row">
+  <div class="col" />
+  <div class="col powered">
+    Powered by
+    <a href="https://github.com/ricardomaia/statuspage" target="_blank">
+      GHoSt</a
+    >
+  </div>
+</div>
 
 <style>
   .powered {
@@ -7,19 +14,3 @@
     margin-right: 5px;
   }
 </style>
-
-<div class="row">
-  <div class="col">
-    <a
-      class="github-button"
-      href="https://github.com/ricardomaia"
-      data-size="large"
-      data-show-count="true"
-      aria-label="Follow @ricardomaia on GitHub">@ricardomaia</a>
-  </div>
-  <div class="col powered">
-    Powered by
-    <a href="https://github.com/ricardomaia/statuspage" target="_blank">
-      GHoSt</a>
-  </div>
-</div>
